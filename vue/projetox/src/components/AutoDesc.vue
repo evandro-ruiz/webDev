cc9ed10fef12bbece0930b8e3f86efb4baf9f8ef
evandro-ruiz <evandro@usp.br>
2022-06-09 18:26:19 -0300

    0901

---
+++ vue/projetox/src/components/AutoDesc.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Descrição do automóvel {{ modelo }}</h2>
+    <InfoDesc />
+  </div>
+</template>
+
+<script>
+import InfoDesc from './InfoDesc.vue'
+
+export default {
+  name: 'AutoDesc',
+  components: {
+    InfoDesc
+  },
+  data() {
+    return {
+      modelo: "Porsche 911"
+    }
+  }
+}
+</script>
--- vue/projetox/src/components/PrimoComp.vue
+++ vue/projetox/src/components/PrimoComp.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1>Olá Vue!</h1>
+  <div>
+    <h1>Olá Vue!</h1>
+    <p>Meu nome é {{ nome }}  e meu hobby é {{ hobby }}.</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'PrimoComp',
+  data() {
+    return {
+      nome: "Evandro",
+      hobby: "ciclismo MTB"
+    }
+  }
 }
 </script>
